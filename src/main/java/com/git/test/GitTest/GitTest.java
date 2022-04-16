571d583cd5b25f3eef0659ff5242727ae2a96c03
xs1234 <xs1234@126.com>
2022-04-16 20:15:14 +0800

    hot-fix-first-commit

--- src/main/java/com/git/test/GitTest/GitTest.java
+++ src/main/java/com/git/test/GitTest/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git");
-        System.out.println("hello,git");
-        System.out.println("hello,git");
+        System.out.println("hello,git999988855");
+        System.out.println("hello,git999999999966666");
     }
 }
