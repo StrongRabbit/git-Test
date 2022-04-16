c47fa9cde4017e1c97228841f7228a84c2549ebd
xs1234 <xs1234@126.com>
2022-04-16 20:24:46 +0800

    third_commit

--- src/main/java/com/git/test/GitTest/GitTest.java
+++ src/main/java/com/git/test/GitTest/GitTest.java
@@ -1,5 +1,7 @@
 package com.git.test.GitTest;
 
+import java.util.HashMap;
+
 import java.util.ArrayList;
 
 public class GitTest {
@@ -8,6 +10,7 @@ public class GitTest {
         System.out.println("hello,git");
         System.out.println("hello,git999988855");
         System.out.println("hello,git999999999966666");
+        HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
         ArrayList<String> objects = new ArrayList<>();
     }
 }
