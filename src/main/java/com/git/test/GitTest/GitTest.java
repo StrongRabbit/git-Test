edfd837f1559b283896bd7b8f6523b293e35d758
xs1234 <xs1234@126.com>
2022-04-16 20:20:53 +0800

    hot-fix-second-commit

--- src/main/java/com/git/test/GitTest/GitTest.java
+++ src/main/java/com/git/test/GitTest/GitTest.java
@@ -1,10 +1,13 @@
 package com.git.test.GitTest;
 
+import java.util.ArrayList;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,git999988855");
         System.out.println("hello,git999999999966666");
+        ArrayList<String> objects = new ArrayList<>();
     }
 }
