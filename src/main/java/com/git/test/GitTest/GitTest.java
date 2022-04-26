9ce98e259d329a5e9678ca7821628f9fb60e7ba9
xs1234 <xs1234@126.com>
2022-04-26 19:50:52 +0800

    fourth_commit

--- src/main/java/com/git/test/GitTest/GitTest.java
+++ src/main/java/com/git/test/GitTest/GitTest.java
@@ -3,6 +3,8 @@ package com.git.test.GitTest;
 import java.util.HashMap;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GitTest {
     public static void main(String[] args) {
@@ -13,5 +15,6 @@ public class GitTest {
         HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
         ArrayList<String> objects = new ArrayList<>();
         Set set = new HashSet();
+        HashMap<Object, Object> hashMap = new HashMap<>();
     }
 }
