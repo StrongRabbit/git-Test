8edd6c1ae5e9d3998765fe9083aec59beab5bd46
xs1234 <xs1234@126.com>
2022-07-14 11:31:06 +0800

    fourth_commit

--- src/main/java/com/git/test/GitTest/GitTest.java
+++ src/main/java/com/git/test/GitTest/GitTest.java
@@ -1,20 +1,23 @@
 package com.git.test.GitTest;
 
-import java.util.HashMap;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.*;
 
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
-        System.out.println("hello,git");
-        System.out.println("hello,git999988855");
-        System.out.println("hello,git999999999966666");
         HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
         ArrayList<String> objects = new ArrayList<>();
+        ArrayBlockingQueue<Runnable> runnable = new ArrayBlockingQueue<>(5);
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        ThreadPoolExecutor.AbortPolicy abortPolicy = new ThreadPoolExecutor.AbortPolicy();
+        ThreadPoolExecutor pool
+                = new ThreadPoolExecutor(2, 3, 60, TimeUnit.SECONDS, runnable, threadFactory, abortPolicy);
         Set set = new HashSet();
         HashMap<Object, Object> hashMap = new HashMap<>();
+        System.out.println("6664");
     }
 }
